431a0950e33e4b752f6bafbef00649f6a9e3b651
digital080 <saqensealy@hotmail.com>
2017-02-27 14:58:02 -0400

    Update StackL.cpp

--- StackL.cpp
+++ StackL.cpp
@@ -1,1 +1,20 @@
 #include "StackL.h"
+#include <iostream>
+
+using namespace std;
+
+
+void Stack::push(int k){
+  Node* frontPtr = nullPtr;
+  Node* newPtr = new Node(k);
+  newPtr -> link = frontPtr;
+  frontPtr = newptr;
+  }
+
+void Stack::pop(){
+  int delData = 0;
+  
+  //delData = //save data
+            //remove ptr
+            //link data back to list
+}
